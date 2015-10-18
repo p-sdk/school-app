4e441fb566b982f9fc33450395e6c10904ba08c2
Paul <paul.sdk@icloud.com>
2015-10-18 20:37:23 +0200

    Stop using instance variables in EnrollmentsController

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,8 @@ class EnrollmentsController < ApplicationController
   before_action :require_sign_in
 
   def create
-    @course = Course.find(params[:enrollment][:course_id])
-    current_user.enroll_in @course
-    redirect_to @course
+    course = Course.find(params[:enrollment][:course_id])
+    current_user.enroll_in course
+    redirect_to course
   end
 end
