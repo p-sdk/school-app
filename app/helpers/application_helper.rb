ec527f0b1baac928f3fedbd56be626e1ca517ef2
Paul <paul.sdk@icloud.com>
2017-04-19 20:51:07 +0200

    Improve flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
     Markdown.new(text, *options).to_html.html_safe
   end
+
+  def alert_name(flash_name)
+    return flash_name if %i(success info warning danger).include? flash_name
+    map = Hash.new(:info).merge(notice: :success, alert: :warning, error: :danger)
+    map[flash_name]
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe ApplicationHelper do
       expect(subject.markdown(arg)).to eq "<h1>Header</h1>\n"
     end
   end
+
+  describe '#alert_name' do
+    it 'does not change bootstrap alerts classes' do
+      %i(success info warning danger).each do |name|
+        expect(subject.alert_name(name)).to eq name
+      end
+    end
+
+    it "converts flash names to bootstrap alert names" do
+      expect(subject.alert_name(:notice)).to eq :success
+      expect(subject.alert_name(:foobar)).to eq :info
+      expect(subject.alert_name(:alert)).to eq :warning
+      expect(subject.alert_name(:error)).to eq :danger
+    end
+  end
 end
