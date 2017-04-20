abcc586f6b2cd52a830dff233bbfd2a6ca763fd8
Paul <paul.sdk@icloud.com>
2017-04-20 11:53:20 +0200

    Set default_url_options for mailer in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Lower BCrypt cost
   BCrypt::Engine.cost = BCrypt::Engine::MIN_COST
+
+  # Define default url for mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
