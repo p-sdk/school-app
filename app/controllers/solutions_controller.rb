e2794fb7431a86ec940e27dfb233c2796f4cd85a
Paul <paul.sdk@icloud.com>
2017-05-09 19:16:31 +0200

    Remove unnecessary instance variables

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,24 +1,23 @@
 class SolutionsController < ApplicationController
-  expose_decorated(:solution)
+  expose_decorated(:solution, build_params: :solution_params_for_create)
   expose_decorated(:task, from: :solution)
 
-  before_action :set_create_params, only: :create
   before_action :authenticate_user!
   before_action :require_correct_user, only: :show
   before_action :require_course_teacher, only: %i[edit update destroy]
   before_action :require_correct_student, only: :create
 
   def create
-    if @task.solve content: @content, student: current_user
+    if task.solve content: solution.content, student: current_user
       flash[:success] = 'Rozwiązanie zostało wysłane'
     else
       flash[:danger] = 'Nie udało się wysłać rozwiązania'
     end
-    redirect_to [@task.course, @task]
+    redirect_to [task.course, task]
   end
 
   def update
-    if solution.update(solution_params)
+    if solution.update(solution_params_for_update)
       flash[:success] = 'Rozwiązanie zostało ocenione'
       redirect_to solution
     else
@@ -38,12 +37,11 @@ class SolutionsController < ApplicationController
     task.course
   end
 
-  def set_create_params
-    @task = Task.find(params[:solution][:task_id])
-    @content = params[:solution][:content]
+  def solution_params_for_create
+    params.require(:solution).permit(:task_id, :content)
   end
 
-  def solution_params
+  def solution_params_for_update
     params.require(:solution).permit(:earned_points)
   end
 
@@ -54,7 +52,7 @@ class SolutionsController < ApplicationController
   end
 
   def require_correct_student
-    return if @task.can_be_solved_by?(current_user)
+    return if task.can_be_solved_by?(current_user)
     deny_access
   end
 end
