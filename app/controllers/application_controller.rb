52d1bcc20e934c45f2d98d04f7435de3888e02fd
Paul <paul.sdk@icloud.com>
2017-11-08 22:01:11 +0100

    Remove unused controller callbacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,24 +15,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  def require_teacher
-    deny_access unless current_user.teacher?
-  end
-
-  def require_course_teacher
-    deny_access unless current_user? course.teacher
-  end
-
-  def require_course_user
-    return if current_user? course.teacher
-    return if course.has_student? current_user
-    deny_access
-  end
-
-  def require_admin
-    deny_access unless current_user.admin?
-  end
-
   def deny_access
     flash[:danger] = 'Odmowa dostępu'
     redirect_to root_path
