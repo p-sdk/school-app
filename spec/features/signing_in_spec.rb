0479b113b80b21258bf36127dc69c06a1ec93252
Paul <paul.sdk@icloud.com>
2017-12-27 20:07:34 +0100

    Flatten authentication spec

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,80 +1,56 @@
 require 'rails_helper'
 
-RSpec.feature 'Authentication', type: :feature do
+RSpec.feature 'Signing in', type: :feature do
   subject { page }
   let(:user) { create :user }
+  let(:protected_page) { edit_user_registration_path }
 
-  describe 'signin' do
-    before { visit new_user_session_path }
+  scenario 'with invalid attributes' do
+    visit root_path
+    click_link 'Zaloguj'
 
-    describe 'page' do
-      it { should have_heading 'Logowanie' }
-    end
+    should have_heading 'Logowanie'
 
-    context 'with invalid information' do
-      before { click_button 'Zaloguj' }
+    click_button 'Zaloguj'
 
-      it 'should display error message' do
-        should have_heading 'Logowanie'
-        should have_warning_message
-      end
-    end
-
-    context 'with valid information' do
-      before do
-        fill_in 'Email', with: user.email
-        fill_in 'Hasło', with: user.password
-        click_button 'Zaloguj'
-      end
-
-      it 'should be signed in' do
-        expect(current_path).to eq root_path
-        should have_link user.name, href: '#'
-        should have_link 'Mój profil', href: user_path(user)
-        should have_link 'Ustawienia', href: edit_user_registration_path
-        should have_link 'Wyloguj', href: destroy_user_session_path
-        should_not have_link 'Zaloguj', href: new_user_session_path
-      end
-
-      describe 'followed by signout' do
-        before { click_link 'Wyloguj' }
-
-        it 'should be signed out' do
-          should have_link 'Zaloguj', href: new_user_session_path
-          should_not have_link user.name
-          should_not have_link 'Mój profil'
-          should_not have_link 'Ustawienia'
-          should_not have_link 'Wyloguj'
-        end
-      end
-    end
+    should have_heading 'Logowanie'
+    should have_warning_message
   end
 
-  describe 'forwarding' do
-    context 'when attempting to visit a protected page' do
-      let(:protected_page) { edit_user_registration_path }
-      before do
-        visit protected_page
-        fill_in 'Email', with: user.email
-        fill_in 'Hasło', with: user.password
-        click_button 'Zaloguj'
-      end
+  scenario 'with valid attributes' do
+    visit root_path
+    click_link 'Zaloguj'
+    fill_in 'Email', with: user.email
+    fill_in 'Hasło', with: user.password
+    click_button 'Zaloguj'
 
-      describe 'after signing in' do
-        it 'should render the desired protected page' do
-          expect(current_path).to eq protected_page
-        end
+    expect(current_path).to eq root_path
+    should have_link user.name, href: '#'
+    should have_link 'Mój profil', href: user_path(user)
+    should have_link 'Ustawienia', href: edit_user_registration_path
+    should have_link 'Wyloguj', href: destroy_user_session_path
+    should_not have_link 'Zaloguj', href: new_user_session_path
 
-        context 'when signing in again' do
-          before do
-            click_link 'Wyloguj'
-            sign_in user
-          end
-          it 'should render default page' do
-            expect(current_path).to eq root_path
-          end
-        end
-      end
-    end
+    click_link 'Wyloguj'
+
+    should have_link 'Zaloguj', href: new_user_session_path
+    should_not have_link user.name
+    should_not have_link 'Mój profil'
+    should_not have_link 'Ustawienia'
+    should_not have_link 'Wyloguj'
+  end
+
+  scenario 'forwarding when attempting to visit a protected page' do
+    visit protected_page
+    fill_in 'Email', with: user.email
+    fill_in 'Hasło', with: user.password
+    click_button 'Zaloguj'
+
+    expect(current_path).to eq protected_page
+
+    click_link 'Wyloguj'
+    sign_in user
+
+    expect(current_path).to eq root_path
   end
 end
