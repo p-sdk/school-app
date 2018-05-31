d4cb6d6a749ea23a9b2685335ed3cda8e7179e2d
p-sdk <paul.sdk@icloud.com>
2018-05-31 16:27:26 +0200

    Configure cleanup of attachments created in tests

--- spec/support/paperclip.rb
+++ spec/support/paperclip.rb
@@ -2,4 +2,7 @@ require 'paperclip/matchers'
 
 RSpec.configure do |config|
   config.include Paperclip::Shoulda::Matchers
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/test_files/"])
+  end
 end
