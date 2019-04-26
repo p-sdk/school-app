01fac1c1c7620727fd378a3d1e02a9457bcb2c56
p-sdk <paul.sdk@icloud.com>
2019-04-26 22:42:41 +0200

    Add email configuration for the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,20 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Setup the mailer config
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    port: ENV['SMTP_PORT'],
+    address: ENV['SMTP_SERVER'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    domain: ENV['APP_HOST'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
